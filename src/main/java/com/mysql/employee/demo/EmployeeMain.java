54cd430a548c40034054fa5910e72bfe28ecadeb
hemeilong <hemeilong@tuzhanai.com>
2020-06-02 12:42:03 +0800

    不采用war包部署到外部tomcat中，使用jar包，spring内置的tomcat部署

--- src/main/java/com/mysql/employee/demo/EmployeeMain.java
+++ src/main/java/com/mysql/employee/demo/EmployeeMain.java
@@ -5,18 +5,11 @@ import org.apache.dubbo.config.spring.context.annotation.DubboComponentScan;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.builder.SpringApplicationBuilder;
-import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
-
 
 @DubboComponentScan({"com.mysql.employee.demo.controller","com.mysql.employee.demo.service.impl"}) //因为这些文件用到了注解，需要加扫描，可扫很多注解
 @SpringBootApplication
 @MapperScan("com.mysql.employee.demo.dao") //扫描Mybatis，即dao文件下接口，该扫描专门用来扫Mybatis
-public class EmployeeMain extends SpringBootServletInitializer {
-    @Override
-    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder){
-        return builder.sources(EmployeeMain.class);
-    }
+public class EmployeeMain {
     public static void main(String[] args) {
         SpringApplication.run(EmployeeMain.class);
     }
