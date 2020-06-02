478bb40a26f6682d4df4af34862851cac8dcfa2f
hemeilong <hemeilong@tuzhanai.com>
2020-06-02 12:12:15 +0800

    提交新增文件提交新增文件

---
+++ src/main/java/com/mysql/employee/demo/service/impl/EmployeesServiceImpl.java
@@ -0,0 +1,79 @@
+package com.mysql.employee.demo.service.impl;
+
+import com.mysql.employee.demo.entity.Employees;
+import com.mysql.employee.demo.dao.EmployeesDao;
+import com.mysql.employee.demo.service.EmployeesService;
+import org.springframework.stereotype.Service;
+
+import javax.annotation.Resource;
+import java.util.List;
+
+/**
+ * (Employees)表服务实现类
+ *
+ * @author makejava
+ * @since 2020-06-01 14:02:28
+ */
+@Service("employeesService")
+public class EmployeesServiceImpl implements EmployeesService {
+    @Resource
+    private EmployeesDao employeesDao;
+
+    /**
+     * 通过ID查询单条数据
+     *
+     * @param empNo 主键
+     * @return 实例对象
+     */
+    @Override
+    public Employees queryById(Integer empNo) {
+        return this.employeesDao.queryById(empNo);
+    }
+
+    /**
+     * 查询多条数据
+     *
+     * @param offset 查询起始位置
+     * @param limit 查询条数
+     * @return 对象列表
+     */
+    @Override
+    public List<Employees> queryAllByLimit(int offset, int limit) {
+        return this.employeesDao.queryAllByLimit(offset, limit);
+    }
+
+    /**
+     * 新增数据
+     *
+     * @param employees 实例对象
+     * @return 实例对象
+     */
+    @Override
+    public Employees insert(Employees employees) {
+        this.employeesDao.insert(employees);
+        return employees;
+    }
+
+    /**
+     * 修改数据
+     *
+     * @param employees 实例对象
+     * @return 实例对象
+     */
+    @Override
+    public Employees update(Employees employees) {
+        this.employeesDao.update(employees);
+        return this.queryById(employees.getEmpNo());
+    }
+
+    /**
+     * 通过主键删除数据
+     *
+     * @param empNo 主键
+     * @return 是否成功
+     */
+    @Override
+    public boolean deleteById(Integer empNo) {
+        return this.employeesDao.deleteById(empNo) > 0;
+    }
+}
